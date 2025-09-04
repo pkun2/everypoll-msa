1517327b80bcbd2cedc1cfcdbbe68406a45a65f1
pkun <pkun2018@gmail.com>
2025-09-04 09:34:18 +0900

    상속 추가

--- authService/src/test/java/com/everypoll/authService/AbstractIntegrationTest.java
+++ authService/src/test/java/com/everypoll/authService/AbstractIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.everypoll.authService;
 
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
--- pollService/src/test/java/com/everypoll/pollService/PollServiceApplicationTests.java
+++ pollService/src/test/java/com/everypoll/pollService/PollServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PollServiceApplicationTests {
+class PollServiceApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
