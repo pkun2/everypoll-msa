19269015fc23e4ea6511eb1b66b3e779e9cb601c
pkun <pkun2018@gmail.com>
2025-11-04 16:15:15 +0900

    일단 localhost 부분 추가

--- auth-init-scripts/init.sql
+++ auth-init-scripts/init.sql
@@ -1,7 +1,9 @@
-CREATE DATABASE IF NOT EXISTS user;
+CREATE DATABASE IF NOT EXISTS user CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE USER 'everyauth_springboot'@'%' IDENTIFIED BY 'AuthSpring2014';
+CREATE USER IF NOT EXISTS 'auth_springboot'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'AuthSpring2014';
+CREATE USER IF NOT EXISTS 'auth_springboot'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'AuthSpring2014';
 
-GRANT ALL PRIVILEGES ON user.* TO 'everyauth_springboot'@'%';
+GRANT ALL PRIVILEGES ON user.* TO 'auth_springboot'@'localhost';
+GRANT ALL PRIVILEGES ON user.* TO 'auth_springboot'@'%';
 
 FLUSH PRIVILEGES;
--- poll-init-scripts/init.sql
+++ poll-init-scripts/init.sql
@@ -1,7 +1,9 @@
--- 일단 poll 데이터베이스 하나 추가
-CREATE DATABASE IF NOT EXISTS poll;
+CREATE DATABASE IF NOT EXISTS poll CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- springboot 사용자용
-CREATE USER 'everypoll_springboot'@'%' IDENTIFIED BY 'Spring2014';
-GRANT ALL PRIVILEGES ON poll.* TO 'everypoll_springboot'@'%';
+CREATE USER IF NOT EXISTS 'poll_springboot'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'Spring2014';
+CREATE USER IF NOT EXISTS 'poll_springboot'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'Spring2014';
+
+GRANT ALL PRIVILEGES ON poll.* TO 'poll_springboot'@'localhost';
+GRANT ALL PRIVILEGES ON poll.* TO 'poll_springboot'@'%';
+
 FLUSH PRIVILEGES;
