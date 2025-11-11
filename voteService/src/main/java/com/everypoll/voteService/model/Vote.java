f529637da494528bb84ecb9d2ec1e77951caa4e9
pkun2014 <pkun2@naver.com>
2025-11-11 16:55:07 +0900

    voteService 환경 설정 완료

---
+++ voteService/src/main/java/com/everypoll/voteService/model/Vote.java
@@ -0,0 +1,7 @@
+package com.everypoll.voteService.model;
+
+import com.everypoll.common.model.BaseTimeEntity;
+
+public class Vote extends BaseTimeEntity {
+    
+}
---
+++ voteService/src/main/java/com/everypoll/voteService/repository/VoteRepository.java
@@ -0,0 +1,7 @@
+package com.everypoll.voteService.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoteRepository extends JpaRepository {
+    
+}
